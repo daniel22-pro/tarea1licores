32e5d17ca45a5fb2de6e026d78b2572b6dd17735
Daniel Guerrero <daniguerrero012@gmail.com>
2025-05-05 19:15:55 -0500

    agrego el archivo requirements.txt y configuro el archivo admin.py

--- dglicores/admin.py
+++ dglicores/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Venta,Inventario,Licor,Cliente
 
 # Register your models here.
+admin.site.register(Venta)
+admin.site.register(Inventario)
+admin.site.register(Licor)
+admin.site.register(Cliente)
+
