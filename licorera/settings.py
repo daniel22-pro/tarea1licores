e452e4870dc6ee100c7abf2882a8573dbe68092e
Daniel Guerrero <daniguerrero012@gmail.com>
2025-05-13 15:03:49 -0500

    adaptamos la base de datos a sqlite y actualizo archivo settings.py

--- licorera/settings.py
+++ licorera/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = 'licorera.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
